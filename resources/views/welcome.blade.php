c7c89a98ff9dc58681d496012117f26fe49776cb
Marshall9600 <marshall.wan96@gmail.com>
2024-11-07 15:04:12 +0800

    Initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -201,7 +201,7 @@
             {{-- @csrf --}}
             {{-- @csrf --}}
             {{-- @csrf --}}
-            @gay
+            @malware
             <button class="uk-modal-close-default" type="button" uk-close></button>
             <div class="uk-margin">
                 <h4><i class="fa-light fa-pencil fa-lg fa-nm"></i> Update Location</h4>
